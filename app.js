3020a6c7db97e1b79b9446ba955262418fcaa0ec
tomorrowsketch <liffdevelop31257014@gmail.com>
2026-09-24 11:57:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -397,7 +397,7 @@ function renderViewMode(data, options = {}) {
 
     ${!selfPreview ? `
     <div class="cta-card">
-      <img src="shareimage.PNG" class="cta-image-left" alt="">
+      <img src="shareimage.webp" class="cta-image-left" alt="">
       <div class="cta-content">
         <h3 class="cta-title">あなたの価値観も共有してみませんか？</h3>
         <p class="cta-text">
